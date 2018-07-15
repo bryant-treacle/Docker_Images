8335737a2a885f0847f864113385efe7a53375cc
pr1malbyt3s <al33w6@gmail.com>
2018-07-15 03:53:57 -0400

    Update rawgraphs_so_install.sh

--- RAWGraphs_SO/rawgraphs_so_install.sh
+++ RAWGraphs_SO/rawgraphs_so_install.sh
@@ -19,21 +19,22 @@ rawgraph_install_option=$(whiptail --title "Docker Install Options" --radiolist 
 
 rawgraph_install_prompt()
 {
-# Unpacking docker container
-    echo "Unpacking rawgraphs docker image"
-    gzip -d rawgraphs_so.tar.gz
-# Loading rawgrapsh docker container
+# Loading rawgraphs docker container
+    echo "Loading rawgraphs_so Docker image."
     docker load --input rawgraphs_so.tar
 }
 
 rawgraph_docker_pull()
 {
+ # Downloading rawgraphs docker container
+    echo "Downloading rawgraphs_so Docker image."
     docker pull pr1malbyt3s/rawgraphs_so
 }
 
 rawgraph_docker_start()
 {
 # Starting Rawgraphs docker container and mounting volume to give apache2 access to the index.html file
+    echo "Starting rawgraphs_so Docker container."
     docker run --name=rawgraphs --volume /var/www/so/rawgraphs/:/rawgraphs/: -d 03a4c76d6e00
     docker exec -it rawgraphs bash -c "cd /raw && cp -R * /rawgraphs/"
 }
@@ -63,13 +64,15 @@ read -r -d '' RAWGRAPHS <<- EOM
                 </Location>
 EOM
 # Creating a new securityonion.conf file with the rawgraphs configs and saving as a variable
-    RAGRAPHS_CONFIG=$(awk -v "n=$nums" -v "s=$RAWGRAPHS" '(NR==n) { print s } 1' /etc/apache2/sites-available/securityonion.conf)
+    RAWGRAPHS_CONFIG=$(awk -v "n=$nums" -v "s=$RAWGRAPHS" '(NR==n) { print s } 1' /etc/apache2/sites-available/securityonion.conf)
 # Creating a backup of the securityonion.conf file
    cp /etc/apache2/sites-available/securityonion.conf /etc/apache2/sites-available/securityonion.conf.bak
 # Replacing old securityonion.conf with new config and restarting Apache2 service
-    echo "$RAGRAPHS_CONFIG=" >  /etc/apache2/sites-available/securityonion.conf
+    echo "$RAWGRAPHS_CONFIG=" >  /etc/apache2/sites-available/securityonion.conf
     systemctl restart apache2
-echo "You can access Rawgraphs using the following url https://localhost/rawgraphs"
+    sleep 2
+    echo ""
+    echo "You can access Rawgraphs using the following url https://localhost/rawgraphs"
 }
 
 ###############
